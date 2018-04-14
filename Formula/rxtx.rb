3d02e829daba8a4c65d83c4d1ad95eaa0ea190e7
Craig Johnston <cjimti@gmail.com>
2018-04-13 22:37:34 -0700

    Brew formula update for rxtx version v1.1.0

--- Formula/rxtx.rb
+++ Formula/rxtx.rb
@@ -1,9 +1,9 @@
 class Rxtx < Formula
   desc "Data queuing and transmission."
   homepage "https://github.com/cjimti/rxtx"
-  url "https://github.com/cjimti/rxtx/releases/download/v1.0.2/rxtx_darwin_amd64.tar.gz"
-  version "1.0.2"
-  sha256 "9e2e364ead109bfa98cda70e3adc771e56868b0ace7afdbdbaff0fb3f3d3ebe2"
+  url "https://github.com/cjimti/rxtx/releases/download/v1.1.0/rxtx_darwin_amd64.tar.gz"
+  version "1.1.0"
+  sha256 "f812e9b4cb55ea4e60d1f558e689c9342674eb77420776796b499b7bce8701e1"
 
   def install
     bin.install "rxtx"
