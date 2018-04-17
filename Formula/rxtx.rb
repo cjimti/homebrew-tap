04671c4e20cbfca00104fc1587817b7d5c0f213f
Craig Johnston <cjimti@gmail.com>
2018-04-16 19:21:45 -0700

    Brew formula update for rtbeat version v1.0.0

--- Formula/rxtx.rb
+++ Formula/rxtx.rb
@@ -1,15 +1,11 @@
 class Rxtx < Formula
-  desc "Data queuing and transmission."
-  homepage "https://github.com/cjimti/rxtx"
-  url "https://github.com/cjimti/rxtx/releases/download/v1.1.0/rxtx_darwin_amd64.tar.gz"
-  version "1.1.0"
-  sha256 "f812e9b4cb55ea4e60d1f558e689c9342674eb77420776796b499b7bce8701e1"
+  desc "Elastic Beat for rxtx."
+  homepage "https://github.com/cjimti/rtbeat"
+  url "https://github.com/cjimti/rtbeat/releases/download/v1.0.0/rtbeat_darwin_amd64.tar.gz"
+  version "1.0.0"
+  sha256 "a36771f72041e978b33f96af88e035eb4cc49b1cbc9a83ecd7fe10f7928b08fd"
 
   def install
-    bin.install "rxtx"
-  end
-
-  test do
-    
+    bin.install "rtbeat"
   end
 end
